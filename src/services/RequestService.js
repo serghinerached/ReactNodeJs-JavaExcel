e6cebcacec6526f087a1fa02c80c9a88b3180705
serghinerached <serghinerached81@gmail.com>
2025-04-28 16:09:10 +0200

    commit 5

--- src/services/RequestService.js
+++ src/services/RequestService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const REQUESTS_REST_API_URL = 'http://localhost:8080/api/tasks/requests';
+//const REQUESTS_REST_API_URL = 'http://localhost:8080/api/tasks/requests';
+const REQUESTS_REST_API_URL = 'http://serghinerached.github.io/tasks/requests';
+
 
 class RequestService {
 
